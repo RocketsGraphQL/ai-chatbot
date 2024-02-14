7fbe5054cc400aeb1443341f4f0082261a711f1a
Kaushik Varanasi <kaushik@Kaushiks-MacBook-Pro.local>
2024-02-14 16:16:47 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Setup Axios once here
+axios.defaults.headers = { Accept: 'application/json' };
+const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL
+});
+export default axiosInstance;
+
+// Find all widget divs
+const widgetDivs = document.querySelectorAll('.nicoraynaud-finance-widget');
+
+// Inject our React App into each class
+widgetDivs.forEach(div => {
+  const root = ReactDOM.createRoot(div);
+  root.render(
+      <React.StrictMode>
+        <App symbol={div.dataset.symbol}/>
+      </React.StrictMode>,
+      div
+  );
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
